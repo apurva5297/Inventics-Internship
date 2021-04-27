7c35039446c502295d28947009a6cd86744bff20
apurva5297 <apurva5297@gmail.com>
2021-04-27 14:23:43 +0530

    API

---
+++ Blog/storage/framework/views/4ee8be8a4ab4bfcc6d7793a7a38574cbd53ed3c3.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>"> 
+                                                                                               
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- Styles -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+<?php if(session('success')): ?>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+<?php echo e(session('success')); ?>
+
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php endif; ?>
+<?php if(session('warning')): ?>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+<?php echo e(session('warning')); ?>
+
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php endif; ?>
+<?php if(session('danger')): ?>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+<?php echo e(session('danger')); ?>
+
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php endif; ?>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
+                    Inventics's Blog
+                </a>&emsp;
+                <a href="#" class="navbar-brand">
+                <?php if(Auth::user()): ?>
+                Welcome back : <?php echo e(Auth::user()->name); ?>
+
+                <?php endif; ?>  
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <?php if(auth()->guard()->guest()): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
+                            </li>
+                            <?php if(Route::has('register')): ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
+                                </li>
+                            <?php endif; ?>
+                        <?php else: ?>
+                        <a class="nav-link " href="<?php echo e(route('role.index')); ?>" >Roles</a>
+                        <a class="nav-link " href="<?php echo e(route('permission.index')); ?>" >Permission</a>
+                        <a  class="nav-link " href="<?php echo e(route('user.role.index')); ?>" >User-Role</a>
+                        <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Category
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="/categories">Category list</a>
+                            <a class="dropdown-item" href="<?php echo e(route('category.create')); ?>">Category Add</a>
+                            <a class="dropdown-item" href="<?php echo e(route('category.deleted')); ?>"> Deleted Category</a>
+                        </div>
+                        </div>
+                        <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Tag
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="/tag">Tag list</a>
+                            <a class="dropdown-item" href="<?php echo e(route('tag.create')); ?>">Tag Add</a>
+                           
+                        </div>
+                        </div>
+                        <a class="nav-link " href="<?php echo e(route('blog.index')); ?>" >Blogs</a>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <?php echo e(Auth::user()->name); ?> <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <?php echo e(__('Logout')); ?>
+
+                                    </a>
+
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                                        <?php echo csrf_field(); ?>
+                                    </form>
+                                </div>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+    $(document).ready(function() {
+      $('.js-example-basic-single').select2();
+    });
+    $(document).ready(function() {
+      $('.js-example-basic-multiple').select2();
+    });
+    </script>
+</body>
+</html><?php /**PATH C:\Users\Apurva\Desktop\LARAVEL\Inventics-Internship\Blog\resources\views/layouts/app.blade.php ENDPATH**/ ?>
---
+++ Blog/storage/framework/views/5cb82e6835dec7223cf51437317322f5bc3c1383.php
@@ -0,0 +1,61 @@
+<?php $__env->startSection('content'); ?>
+<?php
+$index=1;
+?>
+<div class="container">
+<h3 class="title">Blog List  <a style="text-decoration:none;" href="<?php echo e(route('blog.create')); ?>">+</a></h3>
+<div class="row">
+<div class="col-md-2" style=""> </div>
+<div class="col-md-2"><?php echo e($blogs->links()); ?></div>
+<div class="col-md-4"></div>
+<div class="col-md-4" style="margin-bottom:8px";>
+<form action="">
+<div class="row">
+<input class="col-md-6 form-control" style="width" name="searchB" value="<?php echo e($name); ?>" type="text" placeholder="search blog">
+<button class=" btn btn-info btn-sm" style="margin-left:8px";>GO</button>
+</div>
+</form>
+</div>
+</div>
+<table class="table table-striped">
+  <thead>
+  <tr>
+  <th>#</th>
+  <th>Name</th>
+  <th>Category</th>
+  <th>Tags</th>
+  <th>Action</th>
+  </tr>
+  </thead>
+  <tbody>
+  <?php $__currentLoopData = $blogs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $blog): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <tr>
+  <td><?php echo e($index++); ?></td>
+  <td><?php echo e($blog->name); ?></td>
+  <td><?php if($blog->category): ?><?php echo e($blog->category->name); ?><?php endif; ?></td>
+  <td>
+  <?php $__currentLoopData = $blog->tags; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $tag): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <span class="badge badge-warning"><?php echo e($tag->name); ?></span>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+  </td>
+
+  <td>
+  <div class="row">
+  <?php if(Auth::user()): ?>
+  <a class="btn btn-info btn-sm" href="<?php echo e(route('blog.edit',$blog->id)); ?>">edit</a>&nbsp;
+  <a class="btn btn-info btn-sm" href="<?php echo e(route('blog.show',$blog)); ?>">show</a>&nbsp;
+    <form action="<?php echo e(route('blog.destroy',$blog->id)); ?>" method="post">
+    <?php echo csrf_field(); ?>
+    <?php echo method_field('delete'); ?>
+    <button class="btn btn-danger btn-sm" type="submit">delete</button>
+    </form>
+  <?php endif; ?>
+  </div>
+  </td>
+  </tr>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+  </tbody>
+</table>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Apurva\Desktop\LARAVEL\Inventics-Internship\Blog\resources\views/Blog/index.blade.php ENDPATH**/ ?>
