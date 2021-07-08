b09c2e32549988670f9dffd9e9397bff15d8a005
Apurva Srivastava <apurva5297@gmail.com>
2021-07-08 16:33:01 +0530

    Update index.blade.php

--- Blog/resources/views/category/index.blade.php
+++ Blog/resources/views/category/index.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 @section('content')
-@php
+{{-- @php
 $perPage=$categories->perPage();
 $currentPage=$categories->currentPage()-1;
 $index=$perPage*$currentPage+1;
-@endphp
+@endphp --}}
 <div class="container">
-<h3 class="title">Category List  <a style="text-decoration:none;" href="{{route('category.create')}}">+</a></h3>
+<h3 class="title">Category List  <a style="text-decoration:none;" href="">+</a></h3>
 <form action="" method="get">
 <div class="row">
 <div class="col-md-6"></div>
   <div class="col-md-2" style="margin-left:6%";>
-    <input type="text" placeholder="search name" value="{{$name}}"name="searchC" value="" style="margin-bottom:8px"; >
+    <input type="text" placeholder="search name" value=""name="searchC" value="" style="margin-bottom:8px"; >
   </div>
   <div class="col-md-2"style="margin-left:8px";>
-    <input type="text" placeholder="search description" value="{{$description}}"name="searchD" value="" style="margin-bottom:8px;" >
+    <input type="text" placeholder="search description" value=""name="searchD" value="" style="margin-bottom:8px;" >
   </div>
   <div class="col-md-1" style="margin-left:8px";>
     <button type="submit" class="btn btn-info btn-sm">GO</button> 
@@ -32,25 +32,25 @@ $index=$perPage*$currentPage+1;
   </tr>
   </thead>
   <tbody>
-  @foreach($categories as $category)
+  {{-- @foreach($categories as $category) --}}
   <tr>
-  <td>{{$index++}}</td>
-  <td>{{$category->name}}</td>
+  <td></td>
+  <td></td>
  
   <td style="width:30%;">
-  @foreach($category->blogs as $blog)
-  <span class="badge badge-info">{{$blog->name}}</span>
-  @endforeach
+  {{-- @foreach($category->blogs as $blog) --}}
+  <span class="badge badge-info"></span>
+  {{-- @endforeach --}}
   </td>
-  <td style="width:30%;">{{$category->description}}</td>
+  <td style="width:30%;"></td>
   <td>
 
   <div class="row">
-  @if($category->deleted_at)
-  <a class="btn btn-warning btn-sm"href="{{route('category.restore',$category->id)}}">restore</a>
-  @endif
-  <a class="btn btn-info btn-sm" href="{{route('category.edit',$category->id)}}">edit</a>&nbsp;
-  <form action="{{route('category.delete',$category->id)}}" method="post">
+  {{-- @if($category->deleted_at) --}}
+  <a class="btn btn-warning btn-sm"href="">restore</a>
+  {{-- @endif --}}
+  <a class="btn btn-info btn-sm" href="">edit</a>&nbsp;
+  <form action="" method="post">
   @csrf()
   @method('delete')
   <button class="btn btn-danger btn-sm" type="submit">delete</button>
@@ -58,10 +58,13 @@ $index=$perPage*$currentPage+1;
   </div>
   </td>
   </tr>
-  @endforeach
+  {{-- @endforeach --}}
   </tbody>
 </table>
-{{ $categories->links() }}
+{{-- {{ $categories->links() }} --}}
+
+</div>
+@endsection 
 
 </div>
 @endsection 
