59ff3afc0747d3f8dba5ed908ff6533a69c31329
Apurva Srivastava <apurva5297@gmail.com>
2021-07-09 12:04:31 +0530

    Update create.blade.php

--- Blog/resources/views/blog/create.blade.php
+++ Blog/resources/views/blog/create.blade.php
@@ -3,26 +3,26 @@
 
 <div class="container">
 <h3 class="title">Blog Create</h3>
-<form action="{{route('blog.store')}}" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
 @csrf()
   <div class="form-group">
     <label for="exampleInputEmail1">Name</label>
     <input type="text" class="form-control" name="name"  placeholder="name">
   </div>
   @error('name')
-    <span class="text-danger">{{$message}}</span>
+    <span class="text-danger"></span>
 @enderror
 
   <div class="form-group">
     <label for="exampleInputEmail1">Category</label>
     <select name="category" class="js-example-basic-single form-control" >
     <option value="">--select category--</option>
-    @foreach($categories as $category)
-    <option value="{{$category->id}}">{{$category->name}}</option>
-    @endforeach
+   
+    <option value="</option>
+   
     </select>
     @error('category')
-    <span class="text-danger">{{$message}}</span>
+    <span class="text-danger"></span>
     @enderror
   </div>
   <div class="form-group">
@@ -33,19 +33,19 @@
     <label for="exampleInputEmail1">Tags</label>
     <select name="tags[]" class="js-example-basic-multiple form-control" multiple >
     <option value="">--select tag--</option>
-    @foreach($tags as $tag)
-    <option value="{{$tag->id}}">{{$tag->name}}</option>
-    @endforeach
+  
+    <option value=""</option>
+   
     </select>
     @error('tags')
-    <span class="text-danger">{{$message}}</span>
+    <span class="text-danger"></span>
     @enderror
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Description</label>
     <textarea class="form-control" name="description" placeholder="description"></textarea>
     @error('description')
-    <span class="text-danger">{{$message}}</span>
+    <span class="text-danger"></span>
     @enderror
   </div>
  
